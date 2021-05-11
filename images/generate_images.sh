363b5fdfb1aa6642f6bcc3db1ad2f1b409aa9fdf
Yinshangyi <jl.dejaeger@gmail.com>
2021-05-12 00:09:00 +0200

    Create class diagram and readme

---
+++ images/generate_images.sh
@@ -0,0 +1,10 @@
+PLANT_UML_PATH="plantuml.jar"
+PLANT_UML_SRC_FOLDER="src"
+PLANT_UML_OUTPUT_FOLDER="output"
+
+for file in "$PLANT_UML_SRC_FOLDER"/*
+do
+  java -DPLANTUML_LIMIT_SIZE=10000 -jar $PLANT_UML_PATH $file
+  FILE_NAME_NO_EXTENSION="$(cut -d'.' -f1 <<<"$file")"
+  mv "$FILE_NAME_NO_EXTENSION.png" $PLANT_UML_OUTPUT_FOLDER
+done
