1a524db62c151df07993d350d311e4be5db96493
fsz <antoinefer@hotmail.com>
2021-05-13 21:55:15 +0200

    task unit test

---
+++ task/task_test.go
@@ -0,0 +1,29 @@
+package task
+
+import (
+	"testing"
+)
+
+func TestCreateTask(t *testing.T) {
+	task, _ := CreateTask("bash", "my_task")
+
+	if task.name != "my_task" && task.logs != "" && task.status != "" {
+		t.Errorf("Task was not properly created")
+	}
+
+	_, err := CreateTask("unknow", "my_task")
+
+	if err == nil {
+		t.Errorf("Task creation should failed when unknow operator name is passed")
+	}
+}
+
+func TestSetStatus(t *testing.T) {
+	task, _ := CreateTask("bash", "my_task")
+	status := "fail"
+	task.SetStatus(status)
+
+	if task.status != status {
+		t.Errorf("The status was not set properly to status field of Task")
+	}
+}
