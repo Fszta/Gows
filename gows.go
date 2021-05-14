ece3b31f0885958b113ce35de19a925992b141ad
fsz <antoinefer@hotmail.com>
2021-05-15 01:10:47 +0200

    retrieve status from all dag tasks

--- gows.go
+++ gows.go
@@ -3,15 +3,10 @@ package main
 import (
 	"fmt"
 	"gows/dag"
-	"gows/operators"
 	"gows/task"
 )
 
 func main() {
-	operator, _ := operators.NewOperator("bash")
-	operator.SetCmd("ls")
-	output, _ := operator.RunTask()
-	fmt.Println(output)
 
 	task1, _ := task.CreateTask("bash", "task1")
 	task1.Operator.SetCmd("ls -lah")
@@ -25,9 +20,13 @@ func main() {
 	dag.SetDependency(task2, task1)
 
 	task3, _ := task.CreateTask("bash", "task3")
-	task3.Operator.SetCmd("docker ps -aq")
+	task3.Operator.SetCmd("docker ps -aqp")
 	dag.AddTask(task3)
 	dag.SetDependency(task3, task2)
 
 	dag.RunDag()
+
+	for uuid, status := range dag.GetAllTaskStatus() {
+		fmt.Printf("%s : %s \n", uuid.String(), status)
+	}
 }
