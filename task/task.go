210281f7a9cd2489208f0bb2aa476892bbacaad8
Yinshangyi <jl.dejaeger@gmail.com>
2021-05-16 00:22:37 +0200

    Change the operator type sent to the CreateTask function

--- task/task.go
+++ task/task.go
@@ -21,13 +21,8 @@ type Task struct {
 	logs     string
 }
 
-func CreateTask(operatorName string, taskName string) (*Task, error) {
+func CreateTask(operator operators.Operator, taskName string) (*Task, error) {
 	taskUUID := uuid.New()
-	operator, err := operators.NewOperator(operatorName)
-
-	if err != nil {
-		return nil, err
-	}
 
 	return &Task{
 		Operator: operator,
