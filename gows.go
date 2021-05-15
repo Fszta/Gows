c4a23bde866253affe6dd5876a2696173db3edc4
Yinshangyi <jl.dejaeger@gmail.com>
2021-05-16 00:24:57 +0200

    Update the main function for testing the Dag creation

--- gows.go
+++ gows.go
@@ -3,30 +3,41 @@ package main
 import (
 	"fmt"
 	"gows/dag"
+	"gows/operators"
 	"gows/task"
 )
 
 func main() {
 
-	task1, _ := task.CreateTask("bash", "task1")
-	task1.Operator.SetCmd("ls -lah")
-
 	dag, _ := dag.CreateDag("my_dag")
+
+	operator1 := operators.CreateBashOperator()
+	operator1.SetCmd("../sample-src/mockup.sh -n world")
+	task1, _ := task.CreateTask(operator1, "First Bash Task")
 	dag.AddTask(task1)
 
-	task2, _ := task.CreateTask("bash", "task2")
-	task2.Operator.SetCmd("sleep 3 && tree")
+	operator2 := operators.CreateBashOperator()
+	operator2.SetCmd("../sample-src/mockup.sh")
+	operator2.AddArgument("-n", "world")
+	task2, _ := task.CreateTask(operator2, "Second Bash Task")
 	dag.AddTask(task2)
 	dag.SetDependency(task2, task1)
 
-	task3, _ := task.CreateTask("bash", "task3")
-	task3.Operator.SetCmd("docker ps -aqp")
+	operator3 := operators.CreatePythonOperator()
+	operator3.SetSrc("../sample-src/mockup.py")
+	operator3.AddArgument("--name", "world")
+	task3, _ := task.CreateTask(operator3, "Third Python Task")
 	dag.AddTask(task3)
 	dag.SetDependency(task3, task2)
 
-	dag.RunDag()
-
-	for uuid, status := range dag.GetAllTaskStatus() {
-		fmt.Printf("%s : %s \n", uuid.String(), status)
+	for _, value := range dag.GetAllTasks() {
+		taskName := value.GetName()
+		fmt.Println(taskName)
 	}
+
+	// dag.RunDag()
+
+	// for uuid, status := range dag.GetAllTaskStatus() {
+	// 	fmt.Printf("%s : %s \n", uuid.String(), status)
+	// }
 }
