1bdbafd73e214d66bd5cbe7299e85bf95c050efb
fsz <antoinefer@hotmail.com>
2021-06-03 01:00:46 +0200

    remove all subcmd

--- cmd/dag.go
+++ cmd/dag.go
@@ -1,11 +1,6 @@
 package cmd
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"time"
-
 	"github.com/spf13/cobra"
 )
 
@@ -14,18 +9,6 @@ var dagCmd = &cobra.Command{
 	Short: "Manage dags",
 	Run: func(cmd *cobra.Command, args []string) {
 		cmd.Help()
-	},
-}
-
-var addDagCmd = &cobra.Command{
-	Use:   "add",
-	Short: "Add a new dag from json file",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Add dag:", time.Now().String())
-		_, err := http.Get("http://0.0.0.0:2128/add")
-		if err != nil {
-			log.Fatalln(err)
-		}
 	},
 }
 
