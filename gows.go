17e54c50a68a4d734722f399253eea87e4a497f7
fsz <antoinefer@hotmail.com>
2021-06-02 23:36:13 +0200

    execute cmd

--- gows.go
+++ gows.go
@@ -1,49 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"gows/dag"
-	"gows/operators"
-	"gows/task"
+	"com.github/Fszta/gows/cmd"
 )
 
 func main() {
-
-	dag1, _ := dag.CreateDag("my_dag1")
-	operator1 := operators.CreateBashOperator()
-	operator1.SetCmd("ps")
-	task1, _ := task.CreateTask(operator1, "First Bash Task")
-	dag1.AddTask(task1)
-
-	operator2 := operators.CreateBashOperator()
-	operator2.SetCmd("ls")
-	task2, _ := task.CreateTask(operator2, "Second Bash Task")
-	dag1.AddTask(task2)
-	dag1.SetDependency(task2, task1)
-
-	operator3 := operators.CreateBashOperator()
-	operator3.SetCmd("echo 1")
-	task3, _ := task.CreateTask(operator3, "Third Python Task")
-	dag1.AddTask(task3)
-	dag1.SetDependency(task3, task2)
-
-	operator4 := operators.CreateBashOperator()
-	operator4.SetCmd("sleep 1")
-	task4, _ := task.CreateTask(operator4, "Fourth Python Task")
-	dag1.AddTask(task4)
-	dag1.SetDependency(task4, task2)
-
-	dag1.SetScheduler("*/3 * * * * *")
-
-	go func() {
-		err := dag1.DagScheduler.RunScheduler()
-		if err != nil {
-			fmt.Println(err)
-		}
-	}()
-	sum := 0
-	for {
-		sum++
-	}
-
+	cmd.Execute()
 }
