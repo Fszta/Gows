c3e2938c6231f6c042f5ee9ddb5119d3daeecf2f
fsz <antoinefer@hotmail.com>
2021-06-03 01:17:09 +0200

    add name and file required flags

--- cmd/dag_add.go
+++ cmd/dag_add.go
@@ -9,14 +9,26 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	name string
+	file string
+)
+
 var addDagCmd = &cobra.Command{
 	Use:   "add",
 	Short: "Add a new dag from json file",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Add dag:", time.Now().String())
+		fmt.Printf("Add dag: %v based on file %v at %v \n", name, file, time.Now().String())
 		_, err := http.Get("http://0.0.0.0:2128/add")
 		if err != nil {
 			log.Fatalln(err)
 		}
 	},
+}
+
+func init() {
+	addDagCmd.Flags().StringVarP(&name, "name", "n", "", "name of the dag")
+	addDagCmd.MarkFlagRequired("name")
+	addDagCmd.Flags().StringVarP(&file, "file", "f", "", "path of the json file describing dag")
+	addDagCmd.MarkFlagRequired("file")
 }
