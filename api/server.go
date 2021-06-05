757dc977be809bf36c3c500b7955ea7521c36feb
fsz <antoinefer@hotmail.com>
2021-06-05 16:27:59 +0200

    remove unused routes

--- api/server.go
+++ api/server.go
@@ -10,8 +10,6 @@ var port = "2128"
 func Start() {
 	router := Routing()
 	handler := http.Handler(router)
-	http.HandleFunc("/add", AddDag)
-	http.HandleFunc("/list", ListDag)
 	log.Printf("Server started at port %v", port)
 	log.Fatal(http.ListenAndServe("localhost:"+port, handler))
 }
